3fe49b486868473eb3c381dc5e075c88be9c3355
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-04-25 00:21:06 +0100

    Responsive designe

--- src/components/welcome/myLenguajesSkills.js
+++ src/components/welcome/myLenguajesSkills.js
@@ -9,17 +9,18 @@ import Imagen from "../../assests/images/6.png";
 function MyLenguajesSkills() {
   return (
     <div className="profileContent">
+          <img
+        className="imagenMySkills"
+        src={Imagen}
+        alt="Diego Dimitroff Petocff Experience"
+      />
       <div className="textProfileContent">
         <h1> About my language skills</h1>
         My mother tongue is Spanish, but I am able to speak English fluently
         both in reading, writing and speaking! I am currently learning
         Portuguese!.
       </div>
-      <img
-        className="imagenMyExperience"
-        src={Imagen}
-        alt="Diego Dimitroff Petocff Experience"
-      />
+
     </div>
   );
 }
