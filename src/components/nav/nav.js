d1571436ee6ccf812697bce9f34c3893f8e44162
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-04-27 00:26:13 +0100

    FIx Close Botton

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,32 +2,38 @@ import React from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { useState } from "react";
 
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../welcome/welcome.css";
 function Nav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-    console.log("si");
+
     document.getElementById("headerMenu").classList.toggle("show");
   };
 
   return (
     <div id="headerMenu">
-      <button onClick={toggleMenu}>
-        <span className="menuLetters">MENU</span> <span className="navBurguerIcon"><AiOutlineMenu></AiOutlineMenu></span>
+      <button className="headerbuttom" onClick={toggleMenu}>
+        <span className="menuLetters">MENU</span>{" "}
+        <span className="navBurguerIcon">
+          <AiOutlineMenu></AiOutlineMenu>
+        </span>
       </button>
       <nav>
+        <button className="close"  onClick={toggleMenu}>X</button>
+
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
+
           <li>
-          <Link to="/aboutme">About Me</Link>
+            <Link to="/aboutme">About Me</Link>
           </li>
           <li>
-          <Link to="/aboutMyStudies">My Studies</Link>
+            <Link to="/aboutMyStudies">My Studies</Link>
           </li>
           <li>
             <Link to="/articlesList">Some Articles</Link>
@@ -35,10 +41,8 @@ function Nav() {
           <li>
             <Link to="/projectlist">Projects</Link>
           </li>
-          
         </ul>
       </nav>
-  
     </div>
   );
 }
