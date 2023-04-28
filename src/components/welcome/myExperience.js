18703b7e11ce78375c32a95cea2110e7ddd19a91
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-04-28 23:38:24 +0100

    Fix css and add experience

--- src/components/welcome/myExperience.js
+++ src/components/welcome/myExperience.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import "./profile.css";
-import "./myExperience.css"
+import "./myExperience.css";
 
 import Imagen from "../../assests/images/3.png";
 
 import "./welcome.css";
+import { Link } from "react-router-dom";
 function MyExperience() {
   return (
     <div className="profileContent">
@@ -15,18 +16,23 @@ function MyExperience() {
         alt="Diego Dimitroff Petocff Experience"
       />
       <div className="textProfileContentRigth">
-        <h1 > My Experience</h1>
+        <h1> My Experience</h1>
         <ul>
           <li>
-            BACKEND PROGRAMER | Internship NO COUNTRY - Team selected | July -
+            BACKEND PROGRAMER | Internship NO COUNTRY - Team selected | July
+            2022 - Mar 2023
+            <Link to="https://diegodimitroffpetcoff.github.io/Porfolio/images/Certificate-NoCountry-DiegoDimitroffPetcoff.jpg">
+              {" "}
+              - CERTIFICATE -{" "}
+            </Link>
+            <Link to="https://www.nocountry.tech/"> - Company Website - </Link>
           </li>
-          <li>
-            Current. TEAM LIDER | Internship NO COUNTRY | Sep 2022 - Oct 2022·
-          </li>
-          -CERTIFICATE-{" "}
+          <li>T.L. | Internship NO COUNTRY | Sep 2022 - Oct 2022·</li>
+
           <li>
             Data analyst at Google analytics | Internship AUMLA |July 2021 -
             September 2021·
+            <Link to="https://www.aumla.se/"> - Company Website - </Link>
           </li>
         </ul>
       </div>
