41ed282dfce5560baa31e5a61804ca683bfbba53
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-04-28 21:06:30 +0100

    Responsive desig in background

--- src/components/welcome/profile.js
+++ src/components/welcome/profile.js
@@ -9,7 +9,7 @@ function Profile() {
   return (
     <div className="profileContent">
       <div className="textProfileContent">
-        <h1>HI! I'm Diego! Nice to meet you here!</h1> I am Diego, I was born in
+        <h1>How I am</h1> I am Diego, I was born in
         Argentina, in a province called Chaco. In the year 2021 I started
         studying web programming and English! In such a way that everything you
         will see on this website will be knowledge that I acquired approximately
