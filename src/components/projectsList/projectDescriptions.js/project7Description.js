c4e98c4208c33e478bfb769149565c9cb0b14a8f
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-08-11 00:06:36 +0000

    ADD project nails

--- src/components/projectsList/projectDescriptions.js/project7Description.js
+++ src/components/projectsList/projectDescriptions.js/project7Description.js
@@ -4,7 +4,7 @@ import Nav from "../../nav/nav";
 import Imagen from "../../../assests/images/13.png";
 import { Link } from "react-router-dom";
 import {
-
+  AiOutlineDeploymentUnit,
   AiOutlineFile
 } from "react-icons/ai";
 import Welcome from "../../welcome/welcome";
@@ -25,54 +25,23 @@ function ProjectDescription6() {
 
         <span className="projectDescriptionText">
           <div>INTRODUCCION</div>
-          This project is designed to be scalable and compatible with various
-          types of databases, with MongoDB being the initial choice. The primary
-          objective of this project is to provide administrative users with the
-          ability to configure various functionalities. For example, they can
-          showcase different projects through images and offer courses where
-          users with "alumn" status can access videos and complete the entire
-          course to acquire knowledge about nail painting techniques.
+          This is a project completely designed by me. Selection of compatible colors and CSS construction.
+This project is aimed at a client who wants to offer courses for teaching nail painting. In addition to this, you can show the work you do with images that are stored in a different database.
+In this sequence, users will be able to log in and then be able to participate in the classes. Each User will have a "student" role and only the administrator will be able to change that status, since only whoever has the administrator role will be able to make substantial changes to the entire web.
+From this visual I manage the Token that I receive from the Back that I have also created. It will be kept saved as a Cookie in order to obtain greater security. As well as sending email and password to the back to log in (which is correctly encrypted)
           <div>The architecture</div>
-          The architecture of this project follows a modular and scalable
-          approach. It is designed to separate concerns and promote code
-          reusability. Here's an overview of the architectural components:
-          <div>MODEL:</div> The model represents the data structure of the
-          application. It defines the schema and interacts with the database. In
-          this project, MongoDB is used as the database, and Mongoose is
-          utilized as an Object Data Modeling (ODM) library to define and work
-          with the data models. Controller: The controller handles the logic and
-          business operations of the application. It receives requests from the
-          routes and interacts with the necessary services to process the data.
-          It is responsible for handling input validation, calling the
-          appropriate services, and returning the response to the client.
-          <div>SERVICES:</div> The service layer contains the core business
-          logic of the application. It encapsulates reusable functions and
-          performs complex operations. Services interact with the models and
-          other external services as needed. They handle tasks such as data
-          manipulation, data retrieval, and external API integration.
-          <div>ROUTES:</div> The routes define the endpoints and HTTP methods
-          that the application exposes. They receive incoming requests, validate
-          input, and route the requests to the corresponding controller methods.
-          Each route is responsible for a specific resource or functionality in
-          the application. Server: The server file is responsible for setting up
-          the Express application, configuring middleware, and connecting to the
-          database. It initializes the necessary components, such as routes and
-          database connections, and starts the server to listen for incoming
-          requests. By separating concerns into distinct components, this
-          architecture promotes code organization, maintainability, and
-          scalability. It allows for easy addition of new features, modification
-          of existing functionality, and testing of individual components.
+          Regarding the architecture, I have made the difference between reusable components and containers that will show the corresponding information. Also make a folder with the routes and these will use both the containers and the components.
           <div className="projectDescriptionLinks">
-            {/*  <Link className="projectDescriptionLink" to="/">
+              <Link className="projectDescriptionLink" target="_blank" to="https://schoolproject-nine.vercel.app/">
               <AiOutlineDeploymentUnit></AiOutlineDeploymentUnit> DEPLOY CODE
-            </Link>*/}
+            </Link>
             <Link
               className="projectDescriptionLink"
-              to="https://www.linkedin.com/pulse/my-web-development-project-platform-architectural-diego-leonardo%3FtrackingId=cy46zjxnVqwEezUumsKDcg%253D%253D/?trackingId=cy46zjxnVqwEezUumsKDcg%3D%3D"
+              to="https://www.linkedin.com/posts/diegodimitroffpetcoff_hi-there-as-i-said-a-few-days-ago-activity-7079911327955136512-kZbF?utm_source=share&utm_medium=member_desktop"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <AiOutlineFile></AiOutlineFile> Article that I wrote about this project
+              <AiOutlineFile></AiOutlineFile> About the backend of this project
             </Link>
  
           </div>
