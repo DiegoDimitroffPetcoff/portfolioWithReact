1576f78c07008936f34bc8953d2604564ed73720
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-02-29 01:16:01 +0000

    Add new Fabicon and Contacts

--- src/components/common/profile.jsx
+++ src/components/common/profile.jsx
@@ -1,10 +1,48 @@
+import { Link } from "react-router-dom";
+import {
+  AiFillGithub,
+  AiOutlineLinkedin,
+  AiOutlineMail,
+  AiFillPhone,
+} from "react-icons/ai";
 const Profile = () => {
   return (
     <div className="contentProfile">
-      <div  className="lineContent">
+      <div className="lineContent">
         <h1 data-aos="fade-right">Diego Dimitroff Petcoff</h1>
       </div>
-      <button className="buttonContent">Contact</button>
+
+      <div>
+        <Link
+          className="linkProfile"
+          to="https://github.com/DiegoDimitroffPetcoff"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillGithub></AiFillGithub>
+        </Link>
+        <Link
+          className="linkProfile"
+          to="https://www.linkedin.com/in/diegodimitroffpetcoff/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiOutlineLinkedin></AiOutlineLinkedin>
+        </Link>
+
+        <a className="linkProfile" href="tel:+46769681000">
+          <AiFillPhone></AiFillPhone>
+        </a>
+
+        <a
+          className="linkProfile"
+          href="mailto:diegodimitroffpetcoff@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiOutlineMail></AiOutlineMail>
+        </a>
+      </div>
     </div>
   );
 };
